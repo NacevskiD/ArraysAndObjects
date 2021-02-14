dedb325eaac19da9c8ded55991067b4817d036c8
NacevskiD <nacevskidavid@gmail.com>
2021-02-14 14:07:14 -0600

    Initial commit

---
+++ arrays_and_objects.js
@@ -0,0 +1,254 @@
+/* Lab: write the code requested by lines marked //TODO
+You should NOT modify any of the code that's here already. Add the code requested.   */
+
+console.log('Lab. Please write the code requested at the //TODO markers.')
+
+/* a. Use this JavaScript object. This represents the current position of the International Space Station
+at 1pm on January 12th 2018, fetched from http://api.open-notify.org/iss-now.json.
+ */
+
+let iss_location = {
+    "timestamp": 1515784140,
+    "iss_position":
+        {
+            "latitude": "49.2167",
+            "longitude": "100.5363"
+        },
+    "message": "success"
+}
+
+
+console.log(iss_location.iss_position.latitude)
+console.log(iss_location.iss_position.longitude)
+
+console.log('\n')
+
+
+
+
+
+/* b. Use this JavaScript object of exchange rates relative to Euros.
+The properties are currency symbols, the values are the exchange rates.
+Data source: http://fixer.io/
+* */
+
+
+let rates = {
+    "AUD": 1.5417,
+    "BGN": 1.9558,
+    "BRL": 3.8959,
+    "CAD": 1.5194
+}
+
+rates.CHF = 1.17873
+
+
+let aud = 100 * rates.AUD
+console.log("100 Euros to AUD = " +aud.toFixed(2))
+
+let baseRate = 0
+let currency = ''
+
+for (const rate in rates){
+    if (rates[rate] > baseRate){
+        baseRate = rates[rate]
+        currency = rate
+    }
+}
+
+console.log(`Highest exchange rate is ${currency} with ${baseRate}`)
+
+console.log('\n')
+
+
+
+/* c. Use this JavaScript array of objects of cat owners, and their cats. Source, moderncat.com
+ */
+
+let cats_and_owners = [
+    { name: "Bill Clinton", cat: "Socks" },
+    { name: "Gary Oldman", cat: "Soymilk" },
+    { name: "Katy Perry", cat: "Kitty Purry" },
+    { name: "Snoop Dogg", cat: "Miles Davis" }
+]
+
+console.log('Gary Oldman\'s cat name is ' +cats_and_owners[1].cat)
+
+cats_and_owners.push({name: "Taylor Swift", cat: 'Meredith'})
+
+cats_and_owners.forEach(function (member){
+    console.log(`Owner: ${member.name}, Cat: ${member.cat}`)
+})
+
+console.log('\n')
+
+
+
+
+/* d. Use the following JSON object, describing the Nobel Prize winners in 2017.
+Source http://api.nobelprize.org/v1/prize.json?year=2017
+* */
+
+
+
+
+
+
+let nobel_prize_winners_2017 = {
+    "prizes":[
+        {
+            "year":"2017",
+            "category":"physics",
+            "laureates":[
+                {
+                    "id":"941",
+                    "firstname":"Rainer",
+                    "surname":"Weiss",
+                    "motivation":"\"for decisive contributions to the LIGO detector and the observation of gravitational waves\"",
+                    "share":"2"
+                },
+                {
+                    "id":"942",
+                    "firstname":"Barry C.",
+                    "surname":"Barish",
+                    "motivation":"\"for decisive contributions to the LIGO detector and the observation of gravitational waves\"",
+                    "share":"4"
+                },
+                {
+                    "id":"943",
+                    "firstname":"Kip S.",
+                    "surname":"Thorne",
+                    "motivation":"\"for decisive contributions to the LIGO detector and the observation of gravitational waves\"",
+                    "share":"4"
+                }
+            ]
+        },
+        {
+            "year":"2017",
+            "category":"chemistry",
+            "laureates":[
+                {
+                    "id":"944",
+                    "firstname":"Jacques",
+                    "surname":"Dubochet",
+                    "motivation":"\"for developing cryo-electron microscopy for the high-resolution structure determination of biomolecules in solution\"",
+                    "share":"3"
+                },
+                {
+                    "id":"945",
+                    "firstname":"Joachim",
+                    "surname":"Frank",
+                    "motivation":"\"for developing cryo-electron microscopy for the high-resolution structure determination of biomolecules in solution\"",
+                    "share":"3"
+                },
+                {
+                    "id":"946",
+                    "firstname":"Richard",
+                    "surname":"Henderson",
+                    "motivation":"\"for developing cryo-electron microscopy for the high-resolution structure determination of biomolecules in solution\"",
+                    "share":"3"
+                }
+            ]
+        },
+        {
+            "year":"2017",
+            "category":"medicine",
+            "laureates":[
+                {
+                    "id":"938",
+                    "firstname":"Jeffrey C.",
+                    "surname":"Hall",
+                    "motivation":"\"for their discoveries of molecular mechanisms controlling the circadian rhythm\"",
+                    "share":"3"
+                },
+                {
+                    "id":"939",
+                    "firstname":"Michael",
+                    "surname":"Rosbash",
+                    "motivation":"\"for their discoveries of molecular mechanisms controlling the circadian rhythm\"",
+                    "share":"3"
+                },
+                {
+                    "id":"940",
+                    "firstname":"Michael W.",
+                    "surname":"Young",
+                    "motivation":"\"for their discoveries of molecular mechanisms controlling the circadian rhythm\"",
+                    "share":"3"
+                }
+            ]
+        },
+        {
+            "year":"2017",
+            "category":"literature",
+            "laureates":[
+                {
+                    "id":"947",
+                    "firstname":"Kazuo",
+                    "surname":"Ishiguro",
+                    "motivation":"\"who, in novels of great emotional force, has uncovered the abyss beneath our illusory sense of connection with the world\"",
+                    "share":"1"
+                }
+            ]
+        },
+        {
+            "year":"2017",
+            "category":"peace",
+            "laureates":[
+                {
+                    "id":"948",
+                    "firstname":"International Campaign to Abolish Nuclear Weapons (ICAN)",
+                    "motivation":"\"for its work to draw attention to the catastrophic humanitarian consequences of any use of nuclear weapons and for its ground-breaking efforts to achieve a treaty-based prohibition of such weapons\"",
+                    "share":"1",
+                    "surname":""
+                }
+            ]
+        },
+        {
+            "year":"2017",
+            "category":"economics",
+            "laureates":[
+                {
+                    "id":"949",
+                    "firstname":"Richard H.",
+                    "surname":"Thaler",
+                    "motivation":"\"for his contributions to behavioural economics\"",
+                    "share":"1"
+                }
+            ]
+        }
+    ]
+}
+
+let litLaurete = nobel_prize_winners_2017.prizes[3].laureates[0]
+console.log(litLaurete.firstname + " " + litLaurete.surname)
+
+let physLaurates = nobel_prize_winners_2017.prizes[0].laureates
+console.log('Physics id:')
+physLaurates.forEach(function (member){
+    console.log(member.id)
+})
+
+let prizeCategory = nobel_prize_winners_2017.prizes
+console.log('Prize categories:')
+prizeCategory.forEach(function (member){
+    console.log(member.category)
+})
+
+let totalPrizes = 0
+prizeCategory.forEach(function (member){
+    totalPrizes++
+})
+console.log(`Total prizes = ${totalPrizes}`)
+
+
+let prizes = nobel_prize_winners_2017.prizes
+let lauratesIn2017 = 0
+prizes.forEach(function (member){
+    let category = member.laureates
+
+    category.forEach(function (member){
+        lauratesIn2017++
+    })
+})
+
+console.log(`Lauretes in 2017 = ${lauratesIn2017}`)
